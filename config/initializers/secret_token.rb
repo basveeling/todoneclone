ea65efb246ae79f7c1cd1182d264bbed2ce5ca78
Bas Veeling <b.veeling@nedforce.nl>
2010-12-23 15:16:20 +0100

    Initial commit with User and todo scaffolds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todone::Application.config.secret_token = 'd1683d8fa0e604f7009f3138ece09dc6582176623bb7ac6338247af0e0aa67e5b54abef1a698df7dcb3fdbec73760af14fcd0fa19d2d6663a6b33e6ca8c78b3a'
